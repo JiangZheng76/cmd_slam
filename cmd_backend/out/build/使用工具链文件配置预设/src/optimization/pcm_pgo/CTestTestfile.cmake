4689269a11ca51e6eff17148ce3ef457a9487e04
jiangzheng <2440877322@example.com>
2025-01-16 16:38:40 +0800

    过滤不保存 out/

--- cmd_backend/out/build/使用工具链文件配置预设/src/optimization/pcm_pgo/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sysu/cmd_ws/src/cmd-slam/cmd_backend/src/optimization/pcm_pgo
-# Build directory: /home/sysu/cmd_ws/src/cmd-slam/cmd_backend/out/build/使用工具链文件配置预设/src/optimization/pcm_pgo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("max_clique_finder")
-subdirs("utils")
